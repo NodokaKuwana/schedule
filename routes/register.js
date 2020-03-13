33c5fece812264a34f9f144e789bd436bd0c0969
桑名 のどか <kuwana@tdc.co.jp>
2020-03-13 17:02:01 +0900

    commit

--- routes/register.js
+++ routes/register.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-
 /*post*/
 router.post('/', function (req, res) {
   console.log(req.body.test);
