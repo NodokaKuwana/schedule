4f4d3345a9476952642b82577738cc4aa4a0679a
桑名 のどか <kuwana@tdc.co.jp>
2020-03-13 16:52:38 +0900

    commit

--- app.js
+++ app.js
@@ -4,16 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var databaseRouter = require('./routes/database');
-var bodyParser = require('body-parser')
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,10 +17,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/database', databaseRouter);
 
+//ルーティングの設定
+var indexRouter = require('./routes/index');
+var searchRouter = require('./routes/search');
+var registarRouter = require('./routes/register');
+app.use('/', indexRouter);
+app.use('/search', searchRouter);
+app.use('/register', registarRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/register.js
+++ routes/register.js
@@ -6,4 +6,12 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/*post*/
+router.post('/', function (req, res) {
+  console.log(req.body.test);
+  content = req.body.content;
+  res.send('Request is sended.');
+})
+
 module.exports = router;
