37d787bda279a86e89bfb5fa2d3660b87f5d5968
桑名 のどか <kuwana@tdc.co.jp>
2020-03-16 10:16:30 +0900

    commit

--- routes/update.js
+++ routes/update.js
@@ -1,29 +1,31 @@
-var express = require('express');
-var router = express.Router();
-var pg = require('pg');
+// var express = require('express');
+// var router = express.Router();
+// var pg = require('pg');
+/**
+ * とりあえずクライアント側できるまでおいとく
+ */
+// //DELETEメソッドで予定の削除
+// router.put('/', function (req, res, next) {
+//   var pool = pg.Pool({
+//     database: 'schedule',
+//     user: 'postgres', //ユーザー名はデフォルト以外を利用している人は適宜変更してください。
+//     password: 'postgres', //PASSWORDにはPostgreSQLをインストールした際に設定したパスワードを記述。
+//     host: 'localhost',
+//     port: 5432,
+//   });
+//   pool.connect(function (err, client) {
+//     try {
+//       if (req.query.uuid != null) {
+//         client.query(`DELETE FROM schedule where uuid='${req.query.uuid}'`, function (err, result) {
+//           res.send('Request is sended.');
+//         });
+//       } else {
+//         throw Error();
+//       }
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   });
+// });
 
-//DELETEメソッドで予定の削除
-router.delete('/', function (req, res, next) {
-  var pool = pg.Pool({
-    database: 'schedule',
-    user: 'postgres', //ユーザー名はデフォルト以外を利用している人は適宜変更してください。
-    password: 'postgres', //PASSWORDにはPostgreSQLをインストールした際に設定したパスワードを記述。
-    host: 'localhost',
-    port: 5432,
-  });
-  pool.connect(function (err, client) {
-    try {
-      if (req.query.uuid != null) {
-        client.query(`DELETE FROM schedule where uuid='${req.query.uuid}'`, function (err, result) {
-          res.send('Request is sended.');
-        });
-      } else {
-        throw Error();
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  });
-});
-
-module.exports = router;
+// module.exports = router;
