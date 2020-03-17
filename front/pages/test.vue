d25ee9642c84a7d67ae3be8a738189ccf106b653
桑名 のどか <kuwana@tdc.co.jp>
2020-03-17 10:37:13 +0900

    テーマ変更

--- front/pages/test.vue
+++ front/pages/test.vue
@@ -1,15 +1,28 @@
 <template>
   <v-layout>
     <v-flex class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
-        &#8220;this is test page.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
+        予定を登録
+        <v-sheet height="600">
+          <v-calendar
+            ref="calendar"
+            v-model="value"
+            :weekdays="weekday"
+            :type="type"
+            :events="events"
+            :event-overlap-mode="mode"
+            :event-overlap-threshold="30"
+            :event-color="getEventColor"
+            @change="getEvents"
+          ></v-calendar>
+        </v-sheet>
       </blockquote>
     </v-flex>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn color="primary" nuxt to="/inspire">
+        使ってみる
+      </v-btn>
+    </v-card-actions>
   </v-layout>
 </template>
