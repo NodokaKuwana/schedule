34bc4fe1ead9326c57ee5238c9951efdd9710dfa
桑名 のどか <kuwana@tdc.co.jp>
2020-03-27 16:56:24 +0900

    updateAPI

--- API/app.js
+++ API/app.js
@@ -1,10 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,15 +19,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //ルーティングの設定
-var indexRouter = require('./routes/index');
-var searchRouter = require('./routes/search');
-var registarRouter = require('./routes/register');
-var deleteRouter = require('./routes/delete');
-// var updateRouter = require('./routes/update');
+const indexRouter = require('./routes/index');
+const searchRouter = require('./routes/search');
+const registarRouter = require('./routes/register');
+const deleteRouter = require('./routes/delete');
+const updateRouter = require('./routes/update');
+
+// const updateRouter = require('./routes/update');
 app.use('/', indexRouter);
 app.use('/search', searchRouter);
 app.use('/register', registarRouter);
 app.use('/delete', deleteRouter);
+app.use('/update', updateRouter);
+
 // app.use('/update', updateRouter);
 
 // catch 404 and forward to error handler
--- API/routes/update.js
+++ API/routes/update.js
@@ -1,31 +1,35 @@
-// var express = require('express');
-// var router = express.Router();
-// var pg = require('pg');
-/**
- * とりあえずクライアント側できるまでおいとく
- */
-// //DELETEメソッドで予定の削除
-// router.put('/', function (req, res, next) {
-//   var pool = pg.Pool({
-//     database: 'schedule',
-//     user: 'postgres', //ユーザー名はデフォルト以外を利用している人は適宜変更してください。
-//     password: 'postgres', //PASSWORDにはPostgreSQLをインストールした際に設定したパスワードを記述。
-//     host: 'localhost',
-//     port: 5432,
-//   });
-//   pool.connect(function (err, client) {
-//     try {
-//       if (req.query.uuid != null) {
-//         client.query(`DELETE FROM schedule where uuid='${req.query.uuid}'`, function (err, result) {
-//           res.send('Request is sended.');
-//         });
-//       } else {
-//         throw Error();
-//       }
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   });
-// });
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
 
-// module.exports = router;
+/*post*/
+router.post('/', function (req, res) {
+  //リクエストからデータを取り出す
+  const date = req.body.year + '-' + req.body.month + '-' + req.body.day;
+  const time = req.body.hour + req.body.minute;
+  const content = req.body.content;
+  const uuid=req.body.uuid;
+
+  console.log(date + '  ' + time + '  ' + content)
+  //DBに接続
+  var pool = pg.Pool({
+    database: 'schedule',
+    user: 'postgres', //ユーザー名はデフォルト以外を利用している人は適宜変更してください。
+    password: 'postgres', //PASSWORDにはPostgreSQLをインストールした際に設定したパスワードを記述。
+    host: 'localhost',
+    port: 5432,
+  });
+
+  //DBに登録
+  let regist = pool.connect(function (err, client) {
+    try {
+      client.query(`UPDATE schedule SET date='${date}',time='${time}',content='${content}' where uuid='${uuid}'`)
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  pool.end();
+  res.send('Request is sended.');
+})
+
+module.exports = router;
